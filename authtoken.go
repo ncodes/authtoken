c0c89a690e30fc4e7ddd4a4a6001e4a032dcc959
Kennedy Idialu <kennedyidialu@gmail.com>
2018-08-22 12:47:27 +0100

    Support only Bearer schema

--- authtoken.go
+++ authtoken.go
@@ -3,14 +3,12 @@
 package authtoken
 
 import (
-	"encoding/base64"
 	"errors"
 	"net/http"
 	"strings"
 )
 
 const (
-	BASIC_SCHEMA  string = "Basic "
 	BEARER_SCHEMA string = "Bearer "
 )
 
@@ -23,20 +21,9 @@ func FromRequest(req *http.Request) (string, error) {
 	}
 
 	// Confirm the request is sending Basic Authentication credentials.
-	if !strings.HasPrefix(authHeader, BASIC_SCHEMA) && !strings.HasPrefix(authHeader, BEARER_SCHEMA) {
-		return "", errors.New("Authorization requires Basic/Bearer scheme")
+	if !strings.HasPrefix(authHeader, BEARER_SCHEMA) {
+		return "", errors.New("Authorization requires Bearer scheme")
 	}
 
-	// Get the token from the request header
-	// The first six characters are skipped - e.g. "Basic ".
-	if strings.HasPrefix(authHeader, BASIC_SCHEMA) {
-		str, err := base64.StdEncoding.DecodeString(authHeader[len(BASIC_SCHEMA):])
-		if err != nil {
-			return "", errors.New("Base64 encoding issue")
-		}
-		creds := strings.Split(string(str), ":")
-		return creds[0], nil
-	} else {
-		return authHeader[len(BEARER_SCHEMA):], nil
-	}
+	return authHeader[len(BEARER_SCHEMA):], nil
 }
--- authtoken_test.go
+++ authtoken_test.go
@@ -1,44 +1,44 @@
 package authtoken
 
 import (
-  "testing"
-  "net/http"
+	"net/http"
+	"testing"
 )
 
 func TestBearerFromRequest(t *testing.T) {
-  req, _ := http.NewRequest("GET", "", nil)
-  req.Header.Set("Authorization", "Bearer TOKEN")
-  result, err := FromRequest(req)
+	req, _ := http.NewRequest("GET", "", nil)
+	req.Header.Set("Authorization", "Bearer TOKEN")
+	result, err := FromRequest(req)
 
-  if err != nil {
-    t.Errorf("err = %v want nil", err)
-  }
+	if err != nil {
+		t.Errorf("err = %v want nil", err)
+	}
 
-  if result != "TOKEN" {
-    t.Errorf("FromRequest() = %v want %v", result, "TOKEN")
-  }
+	if result != "TOKEN" {
+		t.Errorf("FromRequest() = %v want %v", result, "TOKEN")
+	}
 }
 
-func TestBasicFromRequest(t *testing.T) {
-  req, _ := http.NewRequest("GET", "", nil)
-  req.Header.Set("Authorization", "Basic VE9LRU4=")
-  result, err := FromRequest(req)
+func TestBasicFromRequestNotAllowed(t *testing.T) {
+	req, _ := http.NewRequest("GET", "", nil)
+	req.Header.Set("Authorization", "Basic VE9LRU4=")
+	result, err := FromRequest(req)
+	expected := "Authorization requires Bearer scheme"
+	if err.Error() != expected {
+		t.Errorf("err = %v want nil", err)
+	}
 
-  if err != nil {
-    t.Errorf("err = %v want nil", err)
-  }
-
-  if result != "TOKEN" {
-    t.Errorf("FromRequest() = %v want %v", result, "TOKEN")
-  }
+	if result != "" {
+		t.Errorf("FromRequest() = %v want %v", result, "")
+	}
 }
 
 func TestEmptyFromRequest(t *testing.T) {
-  req, _ := http.NewRequest("GET", "", nil)
-  expected := "Authorization header required"
-  _, err := FromRequest(req)
+	req, _ := http.NewRequest("GET", "", nil)
+	expected := "Authorization header required"
+	_, err := FromRequest(req)
 
-  if err.Error() != expected {
-    t.Errorf("err = %v want %v", err, expected)
-  }
+	if err.Error() != expected {
+		t.Errorf("err = %v want %v", err, expected)
+	}
 }
